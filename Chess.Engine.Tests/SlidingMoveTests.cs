bd16f64dec916fcb46f3c1ccfeeb8b1f4a70ab0c
Zdavis81 <zdavis81@wgu.edu>
2026-06-10 21:22:09 -0600

    Added Rook, Bishop, and Queen logic, basic bishop/queen tests, with more comprehensive rook tests

---
+++ Chess.Engine.Tests/SlidingMoveTests.cs
@@ -0,0 +1,92 @@
+﻿using Chess.Engine;
+using Xunit;
+
+namespace Chess.Engine.Tests
+{
+    public class SlidingMoveTests
+    {
+        [Fact]
+        public void Rook_OnEmptyBoard_HasFourteenMoves() 
+        {
+        
+            var board = new Board();
+            var from = new Square(4, 4);
+            board.SetPiece(from, new Piece(PieceType.Rook, PieceColor.White));
+
+            var moves = MoveGenerator.GenerateMoves(board, from);
+
+            Assert.Equal(14, moves.Count);
+
+        }
+
+        [Fact]
+        public void Bishop_InCorner_HasSevenMoves() 
+        {
+
+            var board = new Board();
+            var from = new Square(0, 0);
+            board.SetPiece(from, new Piece(PieceType.Bishop, PieceColor.White));
+
+            var moves = MoveGenerator.GenerateMoves(board, from);
+
+            Assert.Equal(7, moves.Count);
+
+        }
+
+        [Fact]
+        public void Queen_InCenterOfEmptyBoard_HasTwentySevenMoves() 
+        {
+
+            var board = new Board();
+            var from = new Square(4, 4);
+            board.SetPiece(from, new Piece(PieceType.Queen, PieceColor.White));
+
+            var moves = MoveGenerator.GenerateMoves(board, from);
+
+            Assert.Equal(27, moves.Count);
+        }
+
+        [Fact]
+        public void Rook_StopsBeforeFriendlyPiece() 
+        {
+
+            var board = new Board();
+            var from = new Square(4, 4);
+            board.SetPiece(from, new Piece(PieceType.Rook, PieceColor.White));
+            board.SetPiece(new Square(4, 6), new Piece(PieceType.Pawn, PieceColor.White));
+
+            var moves = MoveGenerator.GenerateMoves(board, from);
+
+            Assert.Contains(moves, m => m.To.Equals(new Square(4, 5)));
+            Assert.DoesNotContain(moves, m => m.To.Equals(new Square(4, 6)));
+            Assert.DoesNotContain(moves, m => m.To.Equals(new Square(4, 7)));
+        }
+
+        [Fact]
+        public void Rook_CapturesEnemyButNotBeyond() 
+        {
+
+            var board = new Board();
+            var from = new Square(4, 4);
+            board.SetPiece(from, new Piece(PieceType.Rook, PieceColor.White));
+            board.SetPiece(new Square(4, 6), new Piece(PieceType.Pawn, PieceColor.Black));
+
+            var moves = MoveGenerator.GenerateMoves(board, from);
+
+            Assert.Contains(moves, m => m.To.Equals(new Square(4, 6)));
+            Assert.DoesNotContain(moves, m => m.To.Equals(new Square(4, 7)));
+        }
+
+        [Fact]
+        public void Rook_OnStartingSquare_IsBoxedIn() 
+        {
+
+            var board = Board.CreateStartingPosition();
+            var from = new Square(7, 0);
+
+            var moves = MoveGenerator.GenerateMoves(board, from);
+
+            Assert.Empty(moves);
+        }
+    }
+}
--- Chess.Engine/MoveGenerator.cs
+++ Chess.Engine/MoveGenerator.cs
@@ -12,6 +12,22 @@ namespace Chess.Engine
             (1, -2), (1, 2), (2, -1), (2, 1)
         };
 
+        private static readonly (int dr, int dc)[] RookDirections = 
+        {
+            (-1, 0), (1, 0), (0, -1), (0,1)
+        };
+
+        private static readonly (int dr, int dc)[] BishopDirections = 
+        {
+            (-1, -1), (-1, 1), (1, -1), (1, 1)
+        };
+
+        private static readonly (int dr, int dc)[] QueenDirections = 
+        {
+            (-1, 0), (1, 0), (0, -1), (0,1),
+            (-1, -1), (-1, 1), (1, -1), (1, 1)
+        };
+
         public static List<Move> GenerateMoves(Board board, Square from) 
         {
 
@@ -26,12 +42,59 @@ namespace Chess.Engine
             return piece.Value.Type switch
             {
                 PieceType.Knight => GenerateKnightMoves(board, from, piece.Value.Color),
+                PieceType.Rook => GenerateSlidingMoves(board, from, piece.Value.Color, RookDirections),
+                PieceType.Bishop => GenerateSlidingMoves(board, from, piece.Value.Color, BishopDirections),
+                PieceType.Queen => GenerateSlidingMoves(board, from, piece.Value.Color, QueenDirections),
                 _ => new List<Move>()
             
             };
 
         }
 
+        public static List<Move> GenerateSlidingMoves(Board board, Square from, PieceColor color, (int dr, int dc)[] directions) 
+        {
+        
+            var moves = new List<Move>();
+
+            foreach (var (dr, dc) in directions) 
+            {
+            
+                var to = new Square(from.Row + dr, from.Col + dc);
+
+                while (to.isOnBoard) 
+                {
+
+                    Piece? target = board[to];
+
+                    // Check if target square is empty
+                    if (target is null)
+                    {
+                        // If target square is empty continue
+                        moves.Add(new Move(from, to));
+
+                    }
+                    else 
+                    {
+                        // Check if target piece is opposing
+                        if (target.Value.Color != color) 
+                        {
+                            // Enemy occupies target square take
+                            moves.Add(new Move(from, to));
+
+                        }
+                        // Break stops us from capturing our own piece
+                        break;
+                    }
+
+                    to = new Square(to.Row + dr, to.Col + dc);
+
+                }
+
+            }
+
+            return moves;
+
+        }
         public static List<Move> GenerateKnightMoves(Board board, Square from, PieceColor color) 
         {
         
